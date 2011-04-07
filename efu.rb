9a0e39e5068a74cdc6b223b0c81e41e641361a1d
Thorsten Fischer <thorsten@froschi.org>
2011-04-07 18:33:31 +0200

    Some refinements.
    * Hardcoded stuff seems to work now.
    * Command line options next.
    
    Signed-off-by: Thorsten Fischer <thorsten@froschi.org>

--- efu.rb
+++ efu.rb
@@ -3,17 +3,22 @@
 require 'rubygems'
 require 'mechanize'
 
-default_patterns = %w/avi jpg mp3 mp4 pdf ogg zip/
+DEFAULT_PATTERNS = %w/avi jpg mp3 mp4 pdf ogg zip/
 patterns = Array.new
 
 agent = Mechanize.new
 if patterns.empty? then
-    patterns = default_patterns
+    patterns = DEFAULT_PATTERNS
 end
 
-page = agent.get('http://microbiology.columbia.edu/W3310_2010.html')
+def absolute_url(root, href)
+  URI.parse(root).merge(href)
+end
+
+source_url = 'http://microbiology.columbia.edu/W3310_2010.html'
+page = agent.get(source_url)
 patterns.each do |pattern|
   page.links_with(:href => /\.#{pattern}$/).each do |link|
-    puts link.uri
+    puts absolute_url(source_url, link.href)
   end
 end
